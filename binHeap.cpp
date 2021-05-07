5d2d0c74eeff9128c5993df06e8cc095caa8482d
Francesco Minnocci <ad17fmin@uwcad.it>
2021-05-07 16:26:27 +0200

    sort of finished binHeap checking

--- binHeap.cpp
+++ binHeap.cpp
@@ -1,25 +1,28 @@
-// TODO
-
 #include <iostream>
 
 using namespace std;
 
-constexpr auto dimensione=5;
+constexpr auto dimensione=10;
 
 int isBinHeap(int *a)
 {
-	return ();
+	int i,k=1;
+	for (i = dimensione-1; i >= 0; --i) {
+		if(a[i]>a[(i-1)/2]) k=0;
+	}
+	return k;
 }
 
 int main()
 {
 	int *a=new int[dimensione];
-	cout << "Inserisci un array candidato ad essere uno heap binario:\n";
+	cout << "Inserisci un array candidato ad essere uno heap binario (max):\n";
 	for(int i=0;i<dimensione;i++){
 		scanf("%d",a+i);
 	}
 
-	isBinHeap(a);
+	int out=isBinHeap(a);
+	cout << "risultato: " << endl << out << endl;
 
 	delete[] a;
 	return 0;
